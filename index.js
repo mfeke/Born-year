2dd9845c27712c2c240112a0a0546da9ae33b11d
mfeke <mfekethothulane@gmail.com>
2022-09-10 20:05:02 +0200

    update

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
 
-let button = document.getElementById("btn")
+let button = document.getElementById("btn-1")
 
-button.addEventListener("click", function tempConv(){
-   userInput = document.getElementById("user").value
-
-   document.getElementById("res").innerHTML = userInput *1.8+32
-
-    
+button.addEventListener("click", function ageCal(){
+   // bornYear= document.getElementById("bornYear").value
+   // curYear = document.getElementById("currYear").value
+   userInput = document.getElementById("celsius").value
+   sum = userInput *1.8+32
+   
+   if(sum){
+       alert(sum +" Fahrenheit")
+   }
+   
 })
+// let btn = document.getElementById("btn-2")
+
+// btn.addEventListener("click", function ageCal(){
+  
+//   userInput = document.getElementById("fahrenheit").value
+//   sum = userInput *1.8+32
+ 
+  
+//   if(sum){
+//       alert(sum +" Fahrenheit")
+//   }
+
+
+// })
+// let btn = document.getElementById("btn-1")
+
+// function tempConv(){
+   
+//    userInput = document.getElementById("celsius").value
+//    res =document.getElementById("res").value
+//    res.innerHTML = userInput *1.8+32
+   
+// }
